6269b9db7d6ee77f74f06e69413876ed1b9fc78f
Murari Gupta <murarigupta2367@gmail.com>
2022-10-18 14:15:09 +0545

    creating a circuit breaker library

---
+++ index.ts
@@ -0,0 +1,44 @@
+// index.ts
+
+import { Request, Response } from 'express';
+// import { CircuitBreaker } from './circuit-breaker/CircuitBreaker';
+// import { BreakerOptions } from './circuit-breaker/BreakerOptions';
+
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+// const breaker1 = new CircuitBreaker(
+//   {
+//     method: 'get',
+//     url: 'http://localhost:3000',
+//   },
+//   new BreakerOptions(3, 5, 5000)
+// );
+
+app.get('/', (req: Request, res: Response) => {
+  const numberGenerated = Math.random();
+
+  if (numberGenerated > 0.5) {
+    res.status(200).send({ numberGenerated });
+  } else {
+    res.status(400).send({ numberGenerated });
+  }
+});
+
+// app.get('/bookings', (req: Request, res: Response) => {
+//   const payload = req.body;
+//   console.log(payload);
+
+//   if (payload) {
+//     res.status(200).send({
+//       status: true,
+//       message: 'ticket booked successfully',
+//       dateTime: Date.now(),
+//       booking_status: 'booked',
+//     });
+//   }
+// });
+
+app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
